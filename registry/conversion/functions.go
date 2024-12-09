go-sprout/sprout

chore: refactor to do not use fmt as a variable
do not use `fmt`, that is a common import name for Go std `fmt` package

Use layout like in the Go package.

I faced issues, and frustration by trying to do a simple fmt.Println
for debugging purpose.

registry/conversion/functions.go
@@ -170,19 +170,19 @@ func (cr *ConversionRegistry) ToString(v any) string {
 //
 // Parameters:
 //
-//	fmt string - the date format string.
-//	str string - the date string to parse.
+//	layout string - the date format string.
+//	str    string - the date string to parse.
 //
 // Returns:
 //
 //	time.Time - the parsed date.
-//	error - error if the date string does not conform to the format.
+//	error     - error if the date string does not conform to the format.
 //
 // Example:
 //
 //	{{ "2023-05-04" | toDate "2006-01-02" }} // Output: 2023-05-04 00:00:00 +0000 UTC
-func (cr *ConversionRegistry) ToDate(fmt, str string) (time.Time, error) {
-	return time.ParseInLocation(fmt, str, time.Local)
+func (cr *ConversionRegistry) ToDate(layout, str string) (time.Time, error) {
+	return time.ParseInLocation(layout, str, time.Local)
 }
 
 // ToLocalDate converts a string to a time.Time object based on a format specification
@@ -190,8 +190,8 @@ func (cr *ConversionRegistry) ToDate(fmt, str string) (time.Time, error) {
 //
 // Parameters:
 //
-//	fmt string - the date format string.
-//	str string - the date string to parse.
+//	layout string - the date format string.
+//	str    string - the date string to parse.
 //
 // Returns:
 //
@@ -201,13 +201,13 @@ func (cr *ConversionRegistry) ToDate(fmt, str string) (time.Time, error) {
 // Example:
 //
 //	{{ "2023-05-04" | toLocalDate "2006-01-02" "Europe/Paris" }} // Output: 2023-05-04 00:00:00 +0200 UTC
-func (cr *ConversionRegistry) ToLocalDate(fmt, timezone, str string) (time.Time, error) {
+func (cr *ConversionRegistry) ToLocalDate(layout, timezone, str string) (time.Time, error) {
 	location, err := time.LoadLocation(timezone)
 	if err != nil {
 		return time.Time{}, err
 	}
 
-	return time.ParseInLocation(fmt, str, location)
+	return time.ParseInLocation(layout, str, location)
 }
 
 // ToDuration converts a value to a time.Duration.

ccoveille-forks/go-sprout

registry/conversion/functions.go
@@ -170,19 +170,19 @@ func (cr *ConversionRegistry) ToString(value any) string {
 //
 // Parameters:
 //
-//	fmt string - the date format string.
-//	value string - the date string to parse.
+//	layout string - the date format string.
+//	value    string - the date string to parse.
 //
 // Returns:
 //
 //	time.Time - the parsed date.
-//	error - error if the date string does not conform to the format.
+//	error     - error if the date string does not conform to the format.
 //
 // For an example of this function in a Go template, refer to [Sprout Documentation: toDate].
 //
 // [Sprout Documentation: toDate]: https://docs.atom.codes/sprout/registries/conversion#todate
-func (cr *ConversionRegistry) ToDate(fmt, value string) (time.Time, error) {
-	return time.ParseInLocation(fmt, value, time.Local)
+func (cr *ConversionRegistry) ToDate(layout, value string) (time.Time, error) {
+	return time.ParseInLocation(layout, value, time.Local)
 }
 
 // ToLocalDate converts a string to a time.Time object based on a format specification
@@ -190,8 +190,8 @@ func (cr *ConversionRegistry) ToDate(fmt, value string) (time.Time, error) {
 //
 // Parameters:
 //
-//	fmt string - the date format string.
-//	value string - the date string to parse.
+//	layout string - the date format string.
+//	value    string - the date string to parse.
 //
 // Returns:
 //
@@ -201,13 +201,13 @@ func (cr *ConversionRegistry) ToDate(fmt, value string) (time.Time, error) {
 // For an example of this function in a Go template, refer to [Sprout Documentation: toLocalDate].
 //
 // [Sprout Documentation: toLocalDate]: https://docs.atom.codes/sprout/registries/conversion#tolocaldate
-func (cr *ConversionRegistry) ToLocalDate(fmt, timezone, value string) (time.Time, error) {
+func (cr *ConversionRegistry) ToLocalDate(layout, timezone, value string) (time.Time, error) {
 	location, err := time.LoadLocation(timezone)
 	if err != nil {
 		return time.Time{}, err
 	}
 
-	return time.ParseInLocation(fmt, value, location)
+	return time.ParseInLocation(layout, value, location)
 }
 
 // ToDuration converts a value to a time.Duration.